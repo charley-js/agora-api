d237005dfe714046e9ef9601f7085679c5060aca
charley-js <charleycrowson1997@gmail.com>
2024-05-28 14:12:08 +0100

    This commit will remove unnecessary express.json() middleware and error handling. Removed testing for unrequired request params and queries

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { getAllTopics } = require("./app.controller");
 
 const app = express();
-app.use(express.json());
 
 //End points
 app.get("/api/topics", getAllTopics);
@@ -11,26 +10,6 @@ app.get("/api/topics", getAllTopics);
 
 app.use((req, res, next) => {
   res.status(404).send({ msg: "Invalid endpoint" });
-});
-
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Invalid input" });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ msg: "Internal Server Error" });
 });
 
 module.exports = app;
