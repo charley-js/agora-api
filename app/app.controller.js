c0ecb63a8bf716ae94ee33e50f0bd9c9d86061a0
charley-js <charleycrowson1997@gmail.com>
2024-05-28 15:21:21 +0100

    This commit will create the endpoint for /api, used for retrieving all endpoints, and will pass relevant integration tests

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,9 +1,19 @@
-const { selectAllTopics } = require("./app.model");
+const { selectAllTopics, readEndpoints } = require("./app.model");
 
 exports.getAllTopics = (req, res, next) => {
   selectAllTopics(req)
     .then((topics) => {
       res.status(200).send(topics);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getAllEndpoints = (req, res, next) => {
+  readEndpoints()
+    .then((endpoints) => {
+      res.status(200).send(endpoints);
     })
     .catch((err) => {
       next(err);
